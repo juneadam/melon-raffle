084e94ee7c94060ed76ef94e08219a12b2b83c4d
June Balter <junedoeswork@gmail.com>
2022-12-21 11:53:55 -0600

    updated version2 modular files, now run as expected

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,26 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from customers import get_customers_from_file
+from random import choice
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+run_raffle()
 
 # Hint: remember to import any functions you need from
 # other files or libraries
